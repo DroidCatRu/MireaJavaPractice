8211e90e61fa135bc7fba0c4eace87c2606bf4cd
Artem Koshkin <artko2050@gmail.com>
2019-11-08 17:37:30 +0300

    Some fixes

--- Practice3.1/src/com/droidcatru/Main.java
+++ Practice3.1/src/com/droidcatru/Main.java
@@ -5,11 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	    Shape mySweetCircle = new Circle(3.0, true, "red");
 	    Shape mySweetRect = new Rectangle(2, 5, true, "blue");
-
+        Shape myFavoriteSquare = new Rectangle(4, true, "lavender");
 	    System.out.println(mySweetCircle);
 	    System.out.println("Circle's area: " + mySweetCircle.getArea() + " and perimeter: " + mySweetCircle.getPerimeter());
 
         System.out.println(mySweetRect);
         System.out.println("Rectangle's area: " + mySweetRect.getArea() + " and perimeter: " + mySweetRect.getPerimeter());
+
+        System.out.println(myFavoriteSquare);
+        System.out.println("Square's area: " + myFavoriteSquare.getArea() + " and perimeter: " + myFavoriteSquare.getPerimeter());
     }
 }
