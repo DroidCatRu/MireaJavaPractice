8a406549aede0133cd5b92a2bdb04a939a28e8fc
Artem Koshkin <artko2050@gmail.com>
2019-11-08 10:42:12 +0300

    Added Rectangle class to Practice 3.1

--- Practice3.1/src/com/droidcatru/Main.java
+++ Practice3.1/src/com/droidcatru/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	    Shape mySweetCircle = new Circle(3.0, true, "red");
+	    Shape mySweetRect = new Rectangle(2, 5, true, "blue");
+
 	    System.out.println(mySweetCircle);
 	    System.out.println("Circle's area: " + mySweetCircle.getArea() + " and perimeter: " + mySweetCircle.getPerimeter());
+
+        System.out.println(mySweetRect);
+        System.out.println("Rectangle's area: " + mySweetRect.getArea() + " and perimeter: " + mySweetRect.getPerimeter());
     }
 }
